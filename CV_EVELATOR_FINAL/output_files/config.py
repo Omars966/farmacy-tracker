520c6b443f591a6ea26fac38a8b01ce500421d34
Omars966 <vilcieniomars@gmail.com>
2025-10-17 08:33:12 +0300

    CVES
    CV_Elevator

---
+++ CV_EVELATOR_FINAL/output_files/config.py
@@ -0,0 +1,7 @@
+INPUTS = "inputs_files"
+OUTPUTS = "output_files"
+PROMPT_FILE = "prompt.md"
+
+API_KEY = "AIzaSyCWH4bAzU4RBAag-CWb2n40dKEg1DhNngc"
+MODEL_NAME = "gemini-2.5-flash"
+TEMPERATURE = 0.2
